44afb5fc036f9f442c0bae1f77bc4c6a6e3a4a87
adebayo <kurdzsamuel@gmail.com>
2021-04-06 07:48:27 +0100

    books data udapted

--- src/app/bookshop/book-list/book-list.component.ts
+++ src/app/bookshop/book-list/book-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Books} from '../books-data';
+import {BookModel} from '../../core/models/book.model';
 
 @Component({
   selector: 'ngx-start-book-list',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
 
+  books: BookModel[] = Books;
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/bookshop/books-data.ts
@@ -0,0 +1,112 @@
+import {BookModel} from '../core/models/book.model';
+
+// all books data are obtained from audibles
+
+export const Books: BookModel[] = [
+  {
+    title: 'The Four Winds',
+    imageUrl: 'https://m.media-amazon.com/images/I/6132R6AHGjL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Kristin Hannah',
+    price: 89
+  },
+  {
+    title: 'Harry Potter and the Order of Phoenix',
+    imageUrl: 'https://m.media-amazon.com/images/I/51ok3DFBrRL._SL320_.jpg',
+    rating: 4.0,
+    author: 'J.K Rowling',
+    price: 89
+  },
+  {
+    title: 'The Promised Land',
+    imageUrl: 'https://m.media-amazon.com/images/I/41bffUhJ4xL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Barack Obama',
+    price: 89
+  },
+  {
+    title: 'Dragon Blood',
+    imageUrl: 'https://m.media-amazon.com/images/I/61VjtL9s7FL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Lindsay Burkler',
+    price: 89
+  },
+  {
+    title: 'Pride and Prejudice',
+    imageUrl: 'https://m.media-amazon.com/images/I/51y-ivhKErL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Jane Austen',
+    price: 89
+  },
+  {
+    title: 'How to train your mind',
+    imageUrl: 'https://m.media-amazon.com/images/I/51LMv04fxLL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Chris Bailey',
+    price: 89
+  },
+  {
+    title: 'How to train your mind',
+    imageUrl: 'https://m.media-amazon.com/images/I/51LMv04fxLL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Chris Bailey',
+    price: 89
+  },
+  {
+    title: 'Atomic Habits',
+    imageUrl: 'https://m.media-amazon.com/images/I/513Y5o-DYtL._SL320_.jpg',
+    rating: 4.3,
+    author: 'James Clear',
+    price: 29
+  },
+  {
+    title: 'The Queen\'s Gambit',
+    imageUrl: 'https://m.media-amazon.com/images/I/51EocHLTvAL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Walter Tevis',
+    price: 89
+  },
+  {
+    title: 'Sleeping Dogs Lie',
+    imageUrl: 'https://m.media-amazon.com/images/I/51b5VCvsctL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Samantha Downing',
+    price: 89
+  },
+  {
+    title: 'The Compound Effect',
+    imageUrl: 'https://m.media-amazon.com/images/I/51F4nsOMnmL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Darren Hardy',
+    price: 89
+  },
+  {
+    title: 'Last Girl Gone',
+    imageUrl: 'https://m.media-amazon.com/images/I/51yqZi5LSKL._SL320_.jpg',
+    rating: 4.0,
+    author: 'J.G. Hetherton',
+    price: 89
+  },
+  {
+    title: 'Ready Player Two',
+    imageUrl: 'https://m.media-amazon.com/images/I/516MN+bKViL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Ernest Cline',
+    price: 89
+  },
+  {
+    title: 'The Stand',
+    imageUrl: 'https://m.media-amazon.com/images/I/51vksp09TJL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Stephen King',
+    price: 89
+  },
+  {
+    title: 'OathBringer',
+    imageUrl: 'https://m.media-amazon.com/images/I/517c9dDm+sL._SL320_.jpg',
+    rating: 4.0,
+    author: 'Brandon Sanderson',
+    price: 89
+  }
+];
+
